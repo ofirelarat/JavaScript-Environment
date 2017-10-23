97b3018113906128eb20c5fa859e9232dcdc9e45
ofirelarat <96ofir11@gmail.com>
2017-10-23 14:24:44 +0300

    add http call with mock api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users',function(req,res){
+    res.json([
+        {"id":1,"firstName":"ofir","lastName":"elarat","email":"132@gmail.com"},
+        {"id":2,"firstName":"njgf","lastName":"afhdh","email":"kbgfdg@gmail.com"},
+        {"id":3,"firstName":"asdf","lastName":"sdgsghs","email":"sg@gmail.com"}
+    ]);
+});
+
 app.listen(port,function(err){
     if(err){
         console.log(err);
